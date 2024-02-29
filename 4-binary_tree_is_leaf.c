16f0f55575677b9b9efa4519fd5997c73abb26e7
iyinolawumi <olatreasure90@gmail.com>
2024-02-29 15:15:19 -0800

    Task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,7 +7,12 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node && !node->left && !node->right)
-        return (1);
-    return (0);
+	if (node == NULL)
+		return (0);
+
+	if ((node->right == NULL) && (node->left == NULL))
+		return (1);
+
+	else
+		return (0);
 }
