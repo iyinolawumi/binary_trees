c18ff173229bcd842075cf8054d926338dc8b3fb
popeopzy <popeopzy@gmail.com>
2024-02-29 06:04:47 +0100

    second commit

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+if (newNode == NULL)
+{
+return (NULL);
+}
+newNode->left = NULL;
+newNode->right = NULL;
+newNode->parent = NULL;
+newNode->n = value;
+if (parent == NULL)
+{
+return (NULL);
+}
+if (parent->left != NULL)
+{
+newNode->left = parent->left;
+parent->left = newNode;
+}
+return newNode;
+}
