ba31f3d7a918b8528ed89ef6fe89b3bb1107d58e
iyinolawumi <olatreasure90@gmail.com>
2024-02-29 15:48:19 -0800

    Task 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -7,7 +7,13 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (node && !node->parent)
-        return (1);
-    return (0);
+	if (node == NULL)
+		return (0);
+
+
+	if (node && !node->parent)
+		return (1);
+
+
+	return (0);
 }
